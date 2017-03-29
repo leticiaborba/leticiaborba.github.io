1b60aa520893e36d69e75e2cd9049e5c9e6aa7a8
joaoandreossi <matusaboh@gmail.com>
2017-03-29 16:49:35 -0300

    loading fix

--- audiovisual/audiovisual.js
+++ audiovisual/audiovisual.js
@@ -1,9 +1,20 @@
-$(window).on("load", function () {
-    
-    $("#loading-spinner").animate({
-        opacity: 0
-    }, function(){
-        $("#loading-spinner").css("display", "none");
-    });
-    
+//preload the spinner button
+function preloadElement(type, className){
+    var newElement = document.createElement(type);
+    var newClass = document.createAttribute('class');
+
+    newClass.value = className;
+    newElement.setAttributeNode(newClass);
+
+    document.getElementsByTagName('body')[0].appendChild(newElement);
+    document.getElementsByTagName('body')[0].removeChild(newElement);            
+}
+
+preloadElement('I', 'fa fa-circle-o-notch');
+
+//hide the loading overlay after the page finished loading
+window.addEventListener("load", function(){
+    var loadingDiv = document.getElementById("loading-div");
+    loadingDiv.className += " active";
+    document.body.className += " active";
 });
